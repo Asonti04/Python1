fa6604748e02f9e4a927635323e103d923c22fd2
Asonti04 <Asontidg@gmail.com>
2023-05-31 01:01:13 +0000

    Added code from the day 2 module

---
+++ DailyCode.py
@@ -0,0 +1,25 @@
+print('Hello World')
+greeting = 'This is my Python Repository!'
+print('My name is Asonti,', greeting)
+age = 19
+I_am_in_school = True
+# We are using the Udemy course to help teach Python
+print(0.000000000000000000000004) #scientific notation
+print(0o163) #octal numbers
+print(0x167) #hexadecimal numbers
+print(6 / 2) #basic division
+print(7 // 2) #integer division
+print(6 % 5) #modulus division how much you have left over. this 5 can go into 6, 1 time with 1 number remaining
+#no numbers can be divided by 0
+print(2 ** 3) #this is a power operator, this is 2 to the power of 3
+print('I am currenlty', age, 'I attened a school called:')
+school = 'Level ' + 'Up ' + 'In ' + 'Tech'
+print(school)
+print('LUIT! ' * 5)
+print('Now lets learn about you')
+print('What school do you attend?')
+school = input()
+print(school, 'sounds like a great school!')
+user = input('Create a username: ')
+email = input('Enter your email: ')
+print('Hey', user, 'I will be sending an invite email to your', email, 'email shortly!')
