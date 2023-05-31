363a867d630ff8eee8a2da0d7d44196b8293403e
Asonti04 <Asontidg@gmail.com>
2023-05-31 03:09:03 +0000

    I created the paycheck calculator

--- SalaryCalc.py
+++ SalaryCalc.py
@@ -1,1 +1,5 @@
-#this project is a salary calculator 
+#This Mini project is a Paycheck calculator
+hours = float(input('How many hours did you work in the last 2 weeks? '))
+rate = float(input('How much do you make a hour? '))
+print('Your incoming check should be $', hours * rate)
+
