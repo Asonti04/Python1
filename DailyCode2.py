cc5b18d2f207253efb958fd6249d09081975a62c
Asonti04 <Asontidg@gmail.com>
2023-06-01 03:26:17 +0000

    half of module 4

--- DailyCode2.py
+++ DailyCode2.py
@@ -12,3 +12,20 @@ if number < 100:
     print('The number is too small')
 else:
     print('Thats how many eggs my chickens lay a month')
+# Trying different Operators
+guess = int(input('Guess the secret number, win a prize, Warning you only get 1 try: '))
+if guess != 6:
+    print('Sorry, that was the incorrect number. You lose')
+else:
+    print('WOW, that must be beginners luck. Your prize is the satisfaction of winning. Hope you love it!')
+#BOOLEAN OPS, you can also us OR. If you use all 3 operators it is in order like: Not, and, Or
+live = input('What country do you live in ? ')
+if not live == 'United States':
+    print('You are not eligible for a consultation')
+else:
+    print('Okay next question')
+    state = input('What state are you in? ')
+    if live == 'United States' and state == 'Virginia':
+        print('You now qualify for a FREE consultation!')
+    else:
+        print('You must pay a $50 fee for your consultation.')
