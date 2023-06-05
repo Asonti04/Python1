4c236d3f14593c4948102112a02ead80919aca9b
Asonti04 <Asontidg@gmail.com>
2023-06-05 03:49:19 +0000

    completed dictionaries & tuples

--- 1TUPLES_DICTIONARY.py
+++ 1TUPLES_DICTIONARY.py
@@ -27,4 +27,34 @@ for description in money:
 me = ('Asonti', 'Student', 2004, [3.27, 5.29, 8.31])
 me[3].append(11.28)
 print(me)
-
+#Dictionaries
+students = {
+    'asonti': 'front row',
+    'johnathan': 'second row',
+    'abby': 'third row'
+    }
+print(students)
+print(students['abby'])
+GPA = {
+    4.0: 'asonti',
+    3.2: 'johnathan',
+    1.7: 'abby'
+}
+print(GPA[4.0])
+#Dictionary Operations
+overall = {}
+overall['keith'] = 1.7
+overall['anna'] = 4.6
+print(overall)
+overall['anna'] = 3.7
+print('grades have now been updated to: ', overall)
+if 'keith' in overall:
+    print('keith\'s overall is: ', overall['keith'])
+for x in overall:
+    print(x)
+for y in overall.keys():
+    print(y)
+for z in overall.values():
+    print(z)
+for name, number in overall.items():
+    print(name, 'achieved an overall of', number)
