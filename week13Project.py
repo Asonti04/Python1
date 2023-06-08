7397fcbe7da0edb1f11388b49e8baffe299627f5
Asonti04 <Asontidg@gmail.com>
2023-06-08 23:07:46 +0000

    week13 project

---
+++ week13Project.py
@@ -0,0 +1,21 @@
+#!bin/bash
+
+import os 
+import glob
+cwd = os.getcwd()
+print(cwd)
+
+ls = os.listdir() 
+print(ls)
+
+Dir = [] #creates empty list
+
+filenames = glob.glob('/home/ec2-user/environment/Python1/*.py') #retrieve filenames
+
+Dir.sort() #Puts file in order by name
+Dir.append(filenames) #adds the filenames to the empty list
+
+for file in filenames:
+    size = os.stat(file)
+    print('File names: ', file, '  File size:  (', size[6], ')')
+    
