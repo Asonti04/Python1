645d6c0c2d701934bac5cd52e6569ebb46fa2355
Asonti04 <Asontidg@gmail.com>
2023-06-09 00:40:34 +0000

    week 13 project

--- week13Project.py
+++ week13Project.py
@@ -2,7 +2,7 @@
 
 import os 
 import glob
-cwd = os.getcwd()
+cwd = os.getcwd() #Prints working directory
 print(cwd)
 
 ls = os.listdir() 
@@ -18,4 +18,3 @@ Dir.append(filenames) #adds the filenames to the empty list
 for file in filenames:
     size = os.stat(file)
     print('File names: ', file, '  File size:  (', size[6], ')')
-    
