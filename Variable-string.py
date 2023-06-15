223ed20c2eb90ae1fb0e9e5399ee4aff21e770ad
Asonti <asontidg@gmail.com>
2023-06-15 01:57:51 +0000

    using format & f string

---
+++ Variable-string.py
@@ -0,0 +1,14 @@
+#format practice
+name1 = 'Tati'
+name2 = 'Ginn'
+name3 = 'Danielle'
+print('My first name is {}, and my last name is {}.'.format (name1, name2))
+
+animal1 = 'sheeps'
+animal2 = 'cows'
+animal3 = 'pigs'
+
+print('On my farm I have 3 types of animals: {}, {}, and {}.'.format (animal1, animal3, animal2))
+
+#f sting practice
+print(f'My sister\'s first name is {name3}, and her last name is {name2}.')
